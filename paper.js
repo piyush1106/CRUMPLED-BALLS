601767c35c3e47082f29c9e9447f986eb0f409f9
piyush1106 <76770482+piyush1106@users.noreply.github.com>
2021-07-06 18:11:32 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,22 @@
+class paper{
+    constructor(x,y,r){
+        var options={
+            isStatic:false,
+               restitution:0.3,
+                   friction:0.5,
+                     density:1.2
+        }
+        this.x=x
+        this.y=y
+        this.r=r
+        this.body=Bodies.circle(x,y,r,options)
+        World.add(world,this.body)
+    }
+    display(){
+         push()
+         translate (this.body.position.x,this.body.position.y)
+         ellipseMode(RADIUS)
+         ellipse(0,0,this.r,this.r)
+         pop()
+    }
+}
